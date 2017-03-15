e0fae5ee3f52b3797eb908895e447d14d2cba89b
jpablos <jpablos2011@gmail.com>
2017-03-14 22:53:36 -0500

    Version Inicial

---
+++ librerias/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+//wan
+define("_servidor", "192.168.100.15");
+define("_odbc", "topsy");
+define("_baseDatos", "App");
+define("_usuario", "wp");
+define("_password", "wp*2017");
+//---------------------------------------
+
+//interfaz
+define("_servidorI", "192.168.100.15");
+define("_odbcI", "topsy");
+define("_baseDatosI", "Topsy");
+define("_usuarioI", "sa");
+define("_passwordI", "");
+
+?>
